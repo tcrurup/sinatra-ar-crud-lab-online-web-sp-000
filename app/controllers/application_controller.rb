628dbdbcc339bd84ea283ed10eb77fac7d0dc92c
Anthony Rurup <tcrurup@gmail.com>
2019-06-18 16:02:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,8 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/articles/:id' do
-    @article = Article.find(:id)
+    @article = Article.find(params[:id])
+    erb :show
   end
   
   post '/articles' do
